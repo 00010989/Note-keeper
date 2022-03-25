90e6c40e4edb16ed946967a05d7bada6b705beb9
serhio2002 <v.korolenko303@gmail.com>
2022-03-25 19:23:08 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# NOTE KEEPER APPLICATION
+
+The note keeper is designed for your everyday convenience. With it help, you will never forget what you need to buy in the grocery shop,  homework you have left, or just your beloved pets will never stay hungry, because all your business will be in one place and will be available at any time for you whenever you wish.
+
+#### Instructions
+1.Download the source code
+
+```js
+git clone [url to the github repo]
+```
+
+or just download the *.zip file 
+
+2.Install deps
+```js
+npm install
+```
+
+3.Run the app
+```js
+node app
+```
+
+#### Deps list
+- express
+- pug
+
+#### Link to github repo
+[url to the github repo]
---
+++ app.js
@@ -0,0 +1,109 @@
+// third party libs
+const express = require('express')
+const app = express()
+
+// node libs
+const fs = require('fs')
+
+app.set('view engine', 'pug')
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    fs.readFile('./data/keeper.json', (err, data) => {
+        if (err) throw err
+
+        const keepers = JSON.parse(data)
+
+        res.render('home', { keepers: keepers })
+    })
+})
+
+app.post('/add', (req, res) => {
+    const formData = req.body
+
+    if (formData.keeper.trim() == '') {
+        fs.readFile('./data/keeper.json', (err, data) => {
+            if (err) throw err
+
+            const keepers = JSON.parse(data)
+
+            res.render('home', { error: true, keepers: keepers })
+        })
+    } else {
+        fs.readFile('./data/keeper.json', (err, data) => {
+            if (err) throw err
+
+            const keepers = JSON.parse(data)
+
+            const keeper = {
+                id: id(),
+                description: formData.keeper,
+                done: false
+            }
+
+            keepers.push(keeper)
+
+            fs.writeFile('./data/keeper.json', JSON.stringify(keepers), (err) => {
+                if (err) throw err
+
+                fs.readFile('./data/keeper.json', (err, data) => {
+                    if (err) throw err
+
+                    const keepers = JSON.parse(data)
+
+                    res.render('home', { success: true, keepers: keepers })
+                })
+            })
+        })
+    }
+})
+
+app.get('/:id/delete', (req, res) => {
+    const id =  req.params.id
+
+    fs.readFile('./data/keeper.json', (err, data) => {
+        if (err) throw err
+
+        const keepers = JSON.parse(data)
+
+        const filteredKeepers = keepers.filter(keeper => keeper.id != id)
+    
+        fs.writeFile('./data/keeper.json', JSON.stringify(filteredKeepers), (err) => {
+            if (err) throw err
+
+        res.render('home', { keepers: filteredKeepers, deleted: true })
+        })
+    })
+})
+
+app.get('/:id/update', (req, res) => {
+    const id = req.params.id
+
+    fs.readFile('./data/keeper.json', (err, data) => {
+        if (err) throw err
+
+        const keepers = JSON.parse(data)
+        const keeper = keepers.filter(keeper => keeper.id == id)[0]
+    
+        const keeperIdx = keepers.indexOf(keeper)
+        const splicedKeeper = keepers.splice(keeperIdx, 1)[0]
+        
+        splicedKeeper.done = true
+
+        keepers.push(splicedKeeper)
+
+        fs.writeFile('./data/keeper.json', JSON.stringify(keepers), (err) => {
+            if (err) throw err
+
+            res.render('home', { keepers: keepers })
+        })
+    })
+})
+
+
+app.listen(3000)
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
